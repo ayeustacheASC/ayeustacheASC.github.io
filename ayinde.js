0cb4764b662aacabf30bd8a0659c6fb02ed137e9
ayeustacheASC <ayeustache@gmail.com>
2016-08-18 12:56:55 -0700

    Ayinde's Website changes

---
+++ ayinde.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	$("#banner2").mouseenter(function(){
+		$("#banner2").css("border-radius","100%")
+	});
+	function setup(){
+		createCanvas(500,500);
+	}
+
+	function draw(){
+		background
+	}
+
+
+
+
+
+
+
+
+});
